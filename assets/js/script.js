fdd90a0e1075a6931335ccb7fe908724a9dc89fa
Yousouf Mustafa Muhammed <yousoufmostafamuslim@gmail.com>
2021-01-26 00:38:39 +0200

    navbar feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+//selectors
 
+const navItemContainer = document.querySelector(".navitem__container");
+const mobileToggle = document.querySelector(".mobile__toggle");
+
+
+//hambutger menu
+
+let activeMenu = () => {
+    navItemContainer.classList.toggle('active')
+    mobileToggle.classList.toggle('isactive')
+    return
+}
+
+
+//event listener
+mobileToggle.addEventListener('click' , activeMenu)
